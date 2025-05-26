c1122dd78e8f955cc04c79083928c9e1267f8160
vineethkumar3 <vineethboddu112233@gmail.com>
2025-05-26 11:12:10 -0400

    changes the cart context file path 2

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-import { CartProvider } from './Pages/CartContext';
+import { CartProvider } from './Pages/CartContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
